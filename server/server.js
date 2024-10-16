b07e2c87362d65015b2aab32c17e8faa65f5bde1
shovonchowdhury <debojjoti550@gmail.com>
2024-10-16 23:49:03 +0600

    solving silly mistakes

--- server/server.js
+++ server/server.js
@@ -30,7 +30,7 @@ const PORT= process.env.PORT || 5000;
 app.use(
 
     cors({
-        origin:'http://localhost:5173',
+        origin: process.env.CLIENT_BASE_URL,
         methods: ['GET','POST','DELETE','PUT'],
         allowedHeaders:[
             "Content-Type",
